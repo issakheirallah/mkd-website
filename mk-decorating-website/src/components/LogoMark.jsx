229945a790559baaee5267c8e93bee17d78bc032
issakheirallah <issakheirallah@gmail.com>
2026-03-11 23:00:25 +0000

    Add SVG logo

--- mk-decorating-website/src/components/LogoMark.jsx
+++ mk-decorating-website/src/components/LogoMark.jsx
@@ -1,14 +1,14 @@
 export default function LogoMark({ className = '', title = 'MK Decorating logo' }) {
   return (
     <svg
-      viewBox="0 0 240 140"
+      viewBox="0 0 144 82"
       role="img"
       aria-label={title}
       className={className}
     >
-      <path d="M20 120L80 20L140 120H20Z" fill="#00aeae" />
-      <path d="M100 120L160 20L220 120H100Z" fill="#00aeae" />
-      <path d="M80 20H160L120 88L80 20Z" fill="#1d3557" />
+      <polygon points="0.816 80.973 72 80.973 36.406 18.688" fill="#00aeae" />
+      <polygon points="72 80.973 143.184 80.973 107.594 18.688" fill="#00aeae" />
+      <polygon points="36.305 1.027 107.59 1.027 72 63.313" fill="#1d3557" />
     </svg>
   )
 }
